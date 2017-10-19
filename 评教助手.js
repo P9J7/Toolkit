6fbb39b8ad565714653727fa4774626b22a0bb51
p9j7 <1002684417@qq.com>
2017-10-19 14:43:50 +0800

    加入试卷填充答案小工具

--- 评教助手.js
+++ 评教助手.js
@@ -1,13 +1,14 @@
 //write by P9J7
+//classId start from 38
 for(var start = 38; start < 100; start++){
-
+//fill 0
     String.prototype.lpad = function(padString, length) {
     var str = this;
     while (str.length <length)
     str = padString + str;
     return str;
     }
-
+//select all choose
     var index = start.toString().lpad(0,10);
     var single = document.getElementsByName(index);
     if(single.length != 0){
@@ -15,6 +16,7 @@ for(var start = 38; start < 100; start++){
     }
 
 }
+//remove waiting-time
 EndTime = new Date();
 document.forms['StDaForm'].submit();
 
